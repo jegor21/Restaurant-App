920d094a6ca5d2db4b585039e08a9dd02374ceca
ervin223 <121256031+ervin223@users.noreply.github.com>
2025-05-09 20:26:39 +0300

    Update ProtectedRoute.js

--- restaurant-app/src/components/ProtectedRoute.js
+++ restaurant-app/src/components/ProtectedRoute.js
@@ -4,17 +4,20 @@ import { UserContext } from '../UserContext';
 import './../styles/ProtectedRoute.css';
 
 const ProtectedRoute = ({ children, requiredRole }) => {
+  // Kasutaja autentimise ja rolli kontrollimine kontekstist
   const { isAuthenticated, user } = useContext(UserContext);
   const [showMessage, setShowMessage] = useState(false);
 
-  
+  // Kui autentimisstaatus on veel määramata
   if (isAuthenticated === null) {
-    return <div>Loading...</div>; 
+    return <div>Laadimine...</div>; 
   }
 
+  // Kui kasutaja pole sisse logitud
   if (!isAuthenticated) {
     if (!showMessage) {
       setShowMessage(true);
+      // Siit suuname kasutaja sisse logimise lehele pärast 5 sekundi ootamist
       setTimeout(() => {
         setShowMessage(false);
         window.location.href = '/login'; 
@@ -22,19 +25,19 @@ const ProtectedRoute = ({ children, requiredRole }) => {
     }
     return (
       <div className="unauth-message">
-        <h2>Access Denied</h2>
-        <p>You must log in to access this page.</p>
-        <p>Redirecting to the login page in 5 seconds...</p>
+        <h2>Juurdepääs keelatud</h2>
+        <p>Peate sisse logima, et sellele lehele pääseda.</p>
+        <p>Suundumine sisse logimise lehele 5 sekundi pärast...</p>
       </div>
     );
   }
 
-  
+  // Kui on määratud vajalik roll ja kasutaja roll ei vasta sellele
   if (requiredRole && user?.role !== requiredRole) {
-    return <Navigate to="/" />;
+    return <Navigate to="/" />; // Suuname kasutaja avalehele
   }
 
-
+  // Kui kõik on korras, siis kuvatakse lastud komponendid
   return children;
 };
 
